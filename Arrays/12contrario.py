5d33ae8f545a959a20c4c689576e3852803ea726
AdryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-04-27 16:58:19 -0300

    atividade 13 e 12

---
+++ Arrays/12contrario.py
@@ -0,0 +1,8 @@
+vetor = []
+
+for x in range(5):
+    vetor.append(input("Digite um nome: "))
+print(vetor)
+
+for y in range(4,-1,-1):
+    print(vetor[y],end=" ")
--- Arrays/atividade13.py
+++ Arrays/atividade13.py
@@ -1,8 +1,33 @@
-a = []
+vetor =[]
+soma = 0
+contador = 0
 
-for x in range(5):
-    a.append(int(input("Digite um numero: ")))
+
+for x in range (5):
+    vetor.append(int(input("Digite um valor: ")))
+    soma = soma+vetor[x]
 
 for y in range(5):
-    if a %2 ==0:
-        print(a[y])
+    if vetor[y]%2 == 0:
+        print(vetor[y],"é numero par")
+
+media = soma/5
+
+for j in range(5):
+    if vetor[j] > media:
+        contador=contador+1
+
+print("A quantidade de numeros acima da media são",contador)
+
+
+maior = vetor[0]
+menor = vetor[0]
+
+for h in range(5):
+    if vetor[h]<menor:
+        menor == vetor[h]
+
+    if vetor[h]>maior:
+        maior=vetor[h]
+
+print("O maior numero é",h)
