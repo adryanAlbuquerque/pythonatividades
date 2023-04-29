902ab0336c93534acc1ef88d565b8a5dac180375
Adryan Albuquerque <adryanfelipe17@hotmail.com>
2023-04-28 23:38:46 -0300

    subindo mais duas atividades

---
+++ porfora/ifelse/maiordeidade.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite sua idade: "))
+
+if n1 >= 18:
+    print("Você tem",n1,"Anos, logo você é maior de idade!")
+else:
+    print("Menor de idade com",n1,"Anos")
---
+++ porfora/ifelse/positivoouneg.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um numero: "))
+
+if n1 >= 0:
+    print(n1,"É um numero positivo!")
+
+else:
+    print(n1,"É um numero negativo!")
