343205e6529b4c93c42b3994db6273c71e91015d
Adryan Albuquerque <adryanfelipe17@hotmail.com>
2023-05-01 18:32:06 -0300

    Atualizando atividades anteriores

--- porfora/for/impares.py
+++ porfora/for/impares.py
@@ -1,7 +1,10 @@
 numeros = []
+contador = 0
+
 for i in range(5):
     numeros.append(int(input("Digite um número: ")))
 
 for numero in numeros:
     if numero % 2 != 0:
-        print(numero)
+        contador += 1
+print("Temos",contador,"numeros ímpares")
--- porfora/for/pares.py
+++ porfora/for/pares.py
@@ -1,8 +1,10 @@
 numeros = []
+pares = 0
 
 for i in range (5):
     numeros.append(int(input("Digite um numero: ")))
 
 for numero in numeros:
     if numero % 2 == 0:
-        print(numero)
+        pares += 1
+print("Temos",pares,"numeros pares")
