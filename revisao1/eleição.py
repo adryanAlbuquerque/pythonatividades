96fc13beaa7602c0ab717cfb9c61536eaa1eb989
AdryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-05-02 17:11:17 -0300

    atividade

---
+++ revisao1/eleição.py
@@ -0,0 +1,22 @@
+eleitores = int(input("Digite o numero de eleitores: "))
+brancos = int(input("Digite o numero de votos brancos: "))
+nulos = int(input("Digite o numero de votos nulos: "))
+validos = int(input("Digite o numero de votos válidos: "))
+
+
+calc1 = (brancos / eleitores) * 100
+calc2 = (nulos / eleitores) * 100
+calc3 = (validos / eleitores) * 100
+
+
+if brancos + nulos + validos / 100 > 100:
+    print("Inválido! houve fraude")
+
+else:
+    print(calc1, "Porcento dos votos branco")
+    print(calc2, "Porcento dos votos nulo")
+    print(calc3, "Porcento dos votos validos")
+    print("Total de eleitores:", eleitores)
+
+
+
---
+++ revisao1/idadeemdias.py
@@ -0,0 +1,8 @@
+num = int(input("Digite sua idade: "))
+meses = int(input("Digite os meses: "))
+dias = int(input("Digite em dias : "))
+
+idade = num * 365
+mes = meses * 30
+
+print(idade + mes + dias,"dias")
